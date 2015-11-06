23fff30195d1029719db63372a0ceadfb22de513
Jair Pereira <pereira.jair@gmail.com>
2015-11-06 14:25:24 -0200

    Correção para o requerias, e atualizado as referências das bibliotecas para outras plataformas.

--- source/build.js
+++ source/build.js
@@ -12,42 +12,50 @@
     optimizeCss: 'standard',
     removeCombined: true,
     paths: {
-        underscore:     "../bower_components/underscore/underscore-min",
-        backbone:       "../bower_components/backbone/backbone-min",
-        webcomponents:  "../bower_components/webcomponentsjs/webcomponents",
-        jquery:         "../bower_components/jquery/dist/jquery.min",
-        jquery_migrate: "../bower_components/jquery-migrate/jquery-migrate.min",
-        moment:         "../bower_components/momentjs/moment",
-        localstorage:   "../bower_components/backbone.browserStorage/backbone.browserStorage",
-        jqueryui:       "../bower_components/jquery-ui/jquery-ui.min",
-        material:       "../bower_components/material-design-lite/material",
-        tinysort:       "../bower_components/tinysort/dist/jquery.tinysort.min",
+        templates:          "../templates",
+        Shared:             "shared",
+        underscore:         "../bower_components/underscore/underscore-min",
+        backbone:           "../bower_components/backbone/backbone-min",
+        webcomponents:      "../bower_components/webcomponentsjs/webcomponents",
+        jquery:             "../bower_components/jquery/dist/jquery.min",
+        jquery_migrate:     "../bower_components/jquery-migrate/jquery-migrate.min",
+        moment:             "../bower_components/momentjs/moment",
+        localstorage:       "../bower_components/backbone.browserStorage/backbone.browserStorage",
+        jqueryui:           "../bower_components/jquery-ui/jquery-ui.min",
+        material:           "../bower_components/material-design-lite/material",
 
+        tinysort:            "../bower_components/tinysort/dist/tinysort.min",
+        tinysort_charorder:  "../bower_components/tinysort/dist/tinysort.charorder.min",
+        tinysort_open:       "../bower_components/tinysort/dist/jquery.tinysort.min",
+        jquery_autogrow:     "../bower_components/autogrow-textarea/jquery.autogrowtextarea.min",
+        tweenmax:            "../bower_components/gsap/src/minified/TweenMax.min",
 
-
-        Shared:         "shared",
-        expressoAPI:    "libs/expresso/expressoAPI",
-        expressoIM:     "libs/expresso/expressoIM",
-        expressoService:"libs/expresso/expressoService",
-        
-
-        im:             "libs/messenger/im",
-        jquery_xmpp:    "libs/jquery.xmpp/jquery.xmpp",
-        
-        autocomplete:   "libs/jquery.backbone.widget/jquery.backbone.widgets",
-        
+        //LANGUAGE FILES
         jqueryui_datepicker_ptBR: "libs/lang/jquery.ui.datepicker-pt-BR",
-        jquery_autogrow: "libs/jquery.autogrow/jquery.autogrowtextarea",
+        moment_ptBR:        "libs/lang/moment-pt-BR",
+
+        expressoAPI:        "libs/expresso/expressoAPI",
+        expressoIM:         "libs/expresso/expressoIM",
+        expressoService:    "libs/expresso/expressoService",
+        im:                 "libs/messenger/im",
+        jquery_xmpp:        "libs/jquery.xmpp/jquery.xmpp",
+        
+
+        //MUST BE CHANGED TO BOWER
+
+        //LOCAL VERSION HAS CHANGES IN CODE
+        autocomplete:       "libs/jquery.backbone.widget/jquery.backbone.widgets",
+        
+        //CHECK VERSIONS
         wijmo:           "libs/jquery.wijmo/jquery.wijmo.min",
         wijdialog:       "libs/jquery.wijmo/jquery.wijmo.wijdialog",
-        linkify:          "libs/linkify/ba-linkify",
+        linkify:         "libs/linkify/ba-linkify",
 
+        //MUST BE REPLACED BY MATERIAL DESIGN
         contextmenu:      "libs/jquery.contextmenu/jquery.contextMenu",
-        tweenmax:         "libs/tweenmax/TweenMax.min",
-        templates:        "../templates"
-
-        //DEVELOPMENT-TEST
-        htmlgl:       "../bower_components/htmlgl/dist/htmlgl.min",
+        
+        //DEVELOPMENT - IN TEST
+        htmlgl:           "../bower_components/htmlgl/dist/htmlgl.min"
     },
     shim: {
           jquery: {
@@ -117,6 +125,20 @@
             ],
             exports: "tinysort"
           },
+          tinysort_charorder: {
+            deps: [
+              "jquery",
+              "tinysort"
+            ],
+          },
+          tinysort_open: {
+            deps: [
+              "jquery",
+              "tinysort_charorder",
+              "tinysort"
+            ],
+            exports: "tinysort_open"
+          },
           jquery_autogrow: {
             deps: [
               "jquery"
@@ -130,6 +152,7 @@
               "wijmo",
               "wijdialog",
               "tinysort",
+              "tinysort_open",
               "contextmenu",
               "linkify",
               "jquery_autogrow"
@@ -158,6 +181,12 @@
             deps: [
               "jquery",
               "jqueryui"
+            ]
+          },
+          moment_ptBR: {
+            deps: [
+              "jquery",
+              "moment"
             ]
           },
           enforceDefine: true,
